341b6002791c7bcf124ef719112c509e04141105
Maina0 <nicolasmaina110@gmail.com>
2022-12-13 10:56:18 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'a' && c != 'a') && c <= 'a')
+				putchar(c);
+			c++;
+		}
+		putchar('\n');
+		return (0);
+}		
+
