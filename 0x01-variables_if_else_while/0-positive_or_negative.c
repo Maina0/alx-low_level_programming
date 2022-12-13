60f36f372ac8dc3e8dae66f9ed1d9643992fe783
Maina0 <nicolasmaina110@gmail.com>
2022-12-13 09:34:18 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,30 @@
-#include <stdlib.h>
+#include <stdib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
- * main - Entry point 
- * Description - Print if random generated number iss +tive, zero or -tive
- * Return: Always 0 (Success)
+ * main - printss if the number i positive, negative or 0
+ * Returns:; Alway 0
  */
-int main(void)
+int maain(void)
 {
-	int n;
+	int n:
 
-	srand(time(0));
+        srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{	
-		printf("%d is negative\n", n);
+        {
+	       printf("%d is positive\n", n);
 	}	
 	else if (n == 0)
-	{	
+	
+	{
 		printf("%d is zero\n", n);
-	}	
-	else if (n < 0)
-	{	
-		printf("%d is position\n", n);
-	}	
-	return (0);
-}	
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+        }
+
+	return (0)/
--- 0x01-variables_if_else_while/README.md
+++
@@ -1,1 +0,0 @@
-0x01-variabless_if_else_while
