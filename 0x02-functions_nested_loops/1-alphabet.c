e07587ad4702a1b499ef8498196b757018b6c5b6
Maina0 <nicolasmaina110@gmail.com>
2022-12-14 14:40:52 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphhabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'a'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
